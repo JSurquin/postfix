c59279f7c733f999c7760522b030f73ce8b93982
jimmylan <jimmylan.srq@gmail.com>
2025-10-22 10:23:07 +0200

    add layouts/question

---
+++ layouts/question.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-slate-800 h-full">
+    <div
+      class="flex flex-col items-center justify-center bg-[url('/question.png')] bg-cover bg-center h-full"
+    >
+      <div class="text-white text-4xl font-bold text-center mx-auto">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
